214eb9763baf276437c43f2c1efdae81e763312d
alecardozat <alecardozat@gmail.com>
2026-02-02 20:49:33 -0500

    feature: check winner

--- main.js
+++ main.js
@@ -22,12 +22,52 @@ function handleMove(e) {
     if (div.classList.contains('selected')) return;
     div.classList.add(nextValue);
     div.classList.add('selected');
-    toggleNextValue()
+    toggleNextValue();
 }
 
 function toggleNextValue() {
+    const winnerPattern = checkWinner();
+    if(winnerPattern){
+        winnerPattern.forEach(id => {
+            document.getElementById(id).classList.add('winner');
+        });
+        document.querySelectorAll('.square').forEach(s => s.removeEventListener('click', handleMove));
+        return;
+    }
     nextValue = nextValue === values[0] ? values[1] : values[0];
-    passValueToCSS()
+    passValueToCSS();
+}
+
+// 0  1  2
+// 3  4  5
+// 6  7  8
+
+function checkWinner(){
+    const boardState = getBoardState();    
+    const winningPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    for(let pattern of winningPatterns){
+        const [a, b, c] = pattern;
+
+        if(boardState[a] && boardState[a] === boardState[b] && boardState[a] === boardState[c]){
+            return pattern;
+        }
+    }
+    return null;
+}
+
+
+
+function getBoardState(){
+    return [...document.querySelectorAll('.square')].map(s => {
+        if(s.classList.contains('cross')) return 'cross';
+        if(s.classList.contains('circle')) return 'circle';
+        return null;
+    })
 }
 
 function passValueToCSS() {
@@ -36,6 +76,8 @@ function passValueToCSS() {
 }
 
 function reset() {
+    nextValue = values[0];
     board.replaceChildren()
+    passValueToCSS();
     printBoard();
 }
